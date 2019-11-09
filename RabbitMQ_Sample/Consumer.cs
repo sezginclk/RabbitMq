d79d61248eff96ab10b23ab90090dee036869d42
Sezgin Çolak <sezgincolak90@gmail.com>
2019-11-09 18:39:02 +0300

    RabbitMq

---
+++ RabbitMQ_Sample/Consumer.cs
@@ -0,0 +1,59 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQ_Sample
+{
+    public class Consumer
+    {
+        private readonly RabbitMQService _rabbitMQService;
+        public string _queue;
+
+        public Consumer(string queueName)
+        {
+            _rabbitMQService = new RabbitMQService();
+
+            var connection = _rabbitMQService.GetRabbitMQConnection();
+            {
+
+
+
+                var channel = connection.CreateModel();
+                {
+
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var _queue = Encoding.UTF8.GetString(body);
+
+
+                        this._queue = _queue;
+                        
+                        
+
+                    };
+
+                    channel.BasicConsume(queueName, false, consumer);
+                    //Console.ReadLine();
+
+                }
+            }
+
+
+
+
+        }
+
+        public string GetMessage()
+        {
+            return _queue;
+        }
+
+    }
+}
---
+++ RabbitMQ_Sample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQ_Sample
+{
+    class Program
+    {
+
+        private static readonly string _queueName = "Sezgin";
+        private static Publisher _publisher;
+        private static Consumer _consumer;
+        static void Main(string[] args)
+        {
+
+            //_publisher = new Publisher(_queueName, "Merhaba Dünya");
+            //_publisher = new Publisher(_queueName, "Merhaba Dünya2");
+            //_publisher = new Publisher(_queueName, "Merhaba Dünya3");
+            //_publisher = new Publisher(_queueName, "Merhaba Dünya4");
+
+            _consumer = new Consumer(_queueName);
+
+            Console.WriteLine("gelen:" + _consumer._queue);
+            Console.ReadLine();
+
+        }
+    }
+}
